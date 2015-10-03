6d979153874e04fff59a6b0b121b4014c4f03d71
Zihuan Xu <xzhflying@163.com>
2015-10-03 22:01:26 +0800

    Add a temperary common navbar

--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -1,16 +1,3 @@
 class PagesController < ApplicationController
   include HighVoltage::StaticPage
-
-  layout :layout_for_page
-
-  private
-
-  def layout_for_page
-    case params[:id]
-    when 'index'
-      'application'
-    else
-      'missionsystem'
-    end
-  end
 end
