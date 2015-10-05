55e69990db3a3e83f01f652891ae91f073ff2528
Zihuan Xu <xzhflying@163.com>
2015-10-05 14:20:16 +0800

    Delete useless validates in models

--- app/models/mission.rb
+++ app/models/mission.rb
@@ -1,7 +1,4 @@
 class Mission < ActiveRecord::Base
-  validates :target, length: { maximum: 30 }
-  validates :description, length: { maximum: 300 }
-
   enum status: { unfinished: 0, finished: 1 }
 
   belongs_to :user, inverse_of: :missions
