5651c4cd0de8ddb7fa861c4f9555368afc47afcd
Zihuan Xu <xzhflying@163.com>
2015-11-01 21:07:50 +0800

    Configurate simple form

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -12,7 +12,9 @@
 //
 //= require jquery
 //= require jquery_ujs
+//= require bootstrap-sprockets
 //= require turbolinks
-//= require_tree .
 //= require moment
 //= require bootstrap-datetimepicker
+//= require simple_form-bootstrap
+//= require_tree .
