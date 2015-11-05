29673c4b5cadb83ccbd7a4eee5f789278a9611f3
Leo <leowangx2013@outlook.com>
2015-11-05 22:07:37 +0800

    Add Content to mission board

--- db/schema.rb
+++ db/schema.rb
@@ -16,14 +16,6 @@ ActiveRecord::Schema.define(version: 20151105131411) do
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
 
-  create_table "comments", force: :cascade do |t|
-    t.integer  "user_id",    index: {name: "index_comments_on_user_id"}
-    t.integer  "mission_id", index: {name: "index_comments_on_mission_id"}
-    t.text     "content"
-    t.datetime "created_at", null: false
-    t.datetime "updated_at", null: false
-  end
-
   create_table "users", force: :cascade do |t|
     t.string   "name",                   null: false
     t.string   "email",                  null: false, index: {name: "index_users_on_email", unique: true}
